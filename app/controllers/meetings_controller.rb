58366bb88f4b8e68b94f276e7444475c9b2212b7
Saroar <saroar9@gmail.com>
2015-11-28 02:04:09 +0300

    Making submit button and create a new gameEvents

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -6,4 +6,15 @@ class MeetingsController < ApplicationController
   def new
     @meeting = Meeting.new
   end
+
+  def create
+    Meeting.create(meeting_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:title, :description, :venue, :game_date, :start_play_time, :end_play_time)
+  end
 end
