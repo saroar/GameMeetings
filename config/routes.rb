ec9a7d3867d4942f506ebf4b842c4fcfa3fe3074
Saroar <saroar9@gmail.com>
2015-11-30 18:57:46 +0300

    create game model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'meetings#index'
   resources :meetings
+
+
+
+
+  resources :games
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127183231) do
+ActiveRecord::Schema.define(version: 20151130150328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "games", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "meetings", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.string   "venue"
     t.date     "game_date"
     t.datetime "start_play_time"
     t.datetime "end_play_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
